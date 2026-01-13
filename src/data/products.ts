e72df52c943a72726e7da7952f00b7d87ac23047
WilmaErikssonIllustrations <wilmaeriksson1@hotmail.com>
2026-01-13 09:21:52 +0100

    created HTML for productcards on productpage

--- src/data/products.ts
+++ src/data/products.ts
@@ -3,28 +3,52 @@ import { Product } from "../models/Product";
 //lista med alla produkt-objekt som vi behöver använda när vi skapar html 
 //ex: products[i].name (kolla models/Product för att se egenskaps-namn)
 export const products: Product[] = [
-    new Product (
-        "/src/assets/produktbilder/surakarameller.jpg", 
+    new Product(
+        "/src/assets/produktbilder/surakarameller.jpg",
         "Zing-Zong Zour",
         "Sensationellt sura!",
         8.90
     ),
-    new Product (
-        "/src/assets/produktbilder/polkaklubba.jpg", 
+    new Product(
+        "/src/assets/produktbilder/polkaklubba.jpg",
         "Swirly Polka",
         "Nostalgiskt god klubba",
         29
     ),
-    new Product (
-        "/src/assets/produktbilder/fruity.jpg", 
+    new Product(
+        "/src/assets/produktbilder/fruity.jpg",
         "Farmors Fruity",
         "Njut av en klassiker",
         8.9
     ),
-    new Product (
-        "/src/assets/produktbilder/fizzy.jpg", 
+    new Product(
+        "/src/assets/produktbilder/fizzy.jpg",
         "FizzyBombz",
         "Med sprudlande smak!",
+        8.9
+    ),
+    new Product(
+        "/src/assets/produktbilder/suraremmar.jpg",
+        "SourSwirls",
+        "Svindlande goda!",
+        8.9
+    ),
+    new Product(
+        "/src/assets/produktbilder/marshmallows.jpg",
+        "Marshmallows",
+        "Fantastiskt fluffiga",
+        8.9
+    ),
+    new Product(
+        "/src/assets/produktbilder/hjartan.jpg",
+        "Lovehearts",
+        "Sprider kärlek",
+        8.9
+    ),
+    new Product(
+        "/src/assets/produktbilder/bonor.jpg",
+        "Deambeans",
+        "Runda, söta & fina",
         8.9
     )
 ];
