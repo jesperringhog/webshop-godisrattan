4905c6a0c3496c9ed8f4f86f7f376a68929cfa19
WilmaErikssonIllustrations <wilmaeriksson1@hotmail.com>
2026-01-19 09:45:43 +0100

    changed price

--- src/data/products.ts
+++ src/data/products.ts
@@ -7,74 +7,74 @@ export const products: Product[] = [
         "/src/assets/produktbilder/surakarameller.jpg",
         "Zing-Zong Zour",
         "Sensationellt sura!",
-        7.90
+        8.9
     ),
     new Product(
         "/src/assets/produktbilder/polkaklubba.jpg",
         "Swirly Polka",
         "Nostalgiskt god klubba",
-        9.90
+        12.9
     ),
     new Product(
         "/src/assets/produktbilder/fruity.jpg",
         "Farmors Fruity",
         "Njut av en klassiker",
-        6.90
+        8.9
     ),
     new Product(
         "/src/assets/produktbilder/fizzy.jpg",
         "FizzyBombz",
         "Med sprudlande smak!",
-        8.90
+        8.9
     ),
     new Product(
         "/src/assets/produktbilder/suraremmar.jpg",
         "SourSwirls",
         "Svindlande goda!",
-        8.90
+        8.9
     ),
     new Product(
         "/src/assets/produktbilder/mashmallows.jpg",
         "Marshmallows",
         "Fantastiskt fluffiga",
-        9.90
+        12.9
     ),
     new Product(
         "/src/assets/produktbilder/hjartan.jpg",
         "Lovehearts",
         "Sprider kärlek",
-        7.90
+        8.9
     ),
     new Product(
         "/src/assets/produktbilder/bonor.jpg",
         "Dreambeans",
         "Runda, söta & fina",
-        6.90
+        8.9
     ),
     new Product(
         "/src/assets/produktbilder/banana-toffee.jpg",
         "Bubbs",
         "Banan & toffee",
-        9.90
+        8.9
     ),
     new Product(
         "/src/assets/produktbilder/saltavral.jpg",
         "Saltvrål",
         "Supersalt!",
-        8.90
+        8.9
     )
     ,
     new Product(
         "/src/assets/produktbilder/hallonlakrits2.jpg",
         "Salta Hallon",
         "Lagom salt & sött",
-        7.90
+        8.9
     )
     ,
     new Product(
         "/src/assets/produktbilder/saltaskallar2.jpg",
         "SaltSkulls",
         "Saltiga favoriter",
-        8.90
+        8.9
     )
 ];
