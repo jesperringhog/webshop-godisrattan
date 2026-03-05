572c38b273cb2773194d71afcb2d82c50a34771b
jesperringhog <jesper.ringhog@gmail.com>
2026-03-05 10:48:04 +0100

    updated vite configuration for build

--- vite.config.ts
+++ vite.config.ts
@@ -1,5 +1,15 @@
 import { defineConfig } from "vite";
+import { resolve } from "path";
 
 export default defineConfig({
-  base: "/",
+  build: {
+    rollupOptions: {
+      input: {
+        main: resolve(__dirname, "index.html"),
+        product: resolve(__dirname, "product.html"),
+        checkout: resolve(__dirname, "checkout.html"),
+        cart: resolve(__dirname, "shoppingCart.html"),
+      },
+    },
+  },
 });
